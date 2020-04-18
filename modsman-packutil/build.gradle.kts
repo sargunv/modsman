3f5d909855b10ad4b7fc00e22c12f9ab49a4cc0d
Sargun Vohra <sargun.vohra@gmail.com>
2020-04-18 00:15:06 -0700

    cleanup

--- modsman-packutil/build.gradle.kts
+++ modsman-packutil/build.gradle.kts
@@ -15,10 +15,6 @@ application {
 
 tasks {
     shadowJar {
-        // defaults to project.name
-//        archiveBaseName.set("${project.name}-fat")
-
-        // defaults to all, so removing this overrides the normal, non-fat jar
         archiveClassifier.set("")
     }
 }
